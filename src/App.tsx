9c67e60d81e52c164c10085f7a88b4325df34312
Brooke <bjp97@cornell.edu>
2026-04-09 17:47:31 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
         ],
     },
 ]);
-//dsdsdsds
+//dsds
 export default function App() {
     return (
         <MantineProvider withGlobalStyles withNormalizeCSS>
