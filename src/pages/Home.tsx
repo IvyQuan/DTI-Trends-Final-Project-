b32b5a06d3dc701e4a8a49eae4d2efb14fd32899
Brooke <bjp97@cornell.edu>
2026-04-13 23:10:46 -0400

    finished GET and connected front/backend
    hard coded previous sessions' data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,29 @@
-const HomePage = () => <h1>Welcome to the Homepage!</h1>;
+import { useEffect, useState } from 'react';
+
+function HomePage() {
+  const [sessions, setSessions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/games')
+      .then(res => res.json())
+      .then(data => setSessions(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to Group Game Night Organizer!</h1>
+      <h2>Previous Sessions</h2>
+      {sessions.map((session: any) => (
+        <div key={session.gameId} style={{ marginLeft: '25px' }}>
+          <p style = {{fontSize: '20px'}}>{session.name} -- Winner: {session.winner}</p>
+          <p>Players: {session.players.join(', ')}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default HomePage;
+
+
+
