ce96a3a21f08cce7ba5a9e90b30cb590168a0ec9
Brooke <bjp97@cornell.edu>
2026-05-04 19:25:34 -0400

    final ui changes

--- server/firebase.js
+++ server/firebase.js
@@ -1,24 +1,13 @@
-import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
-import serviceAccount from './service_account.json';
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 
-const app = initializeApp({
-  credential: cert(serviceAccount),
+initializeApp({
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+  }),
 });
+
 const db = getFirestore();
-
-export { db };
-
-// const { initializeApp, cert } = require('firebase-admin/app');
-// const { getFirestore } = require('firebase-admin/firestore');
-
-// initializeApp({
-//   credential: cert({
-//     projectId: process.env.FIREBASE_PROJECT_ID,
-//     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//     privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
-//   }),
-// });
-
-// const db = getFirestore();
-// module.exports = { db };
+module.exports = { db };
